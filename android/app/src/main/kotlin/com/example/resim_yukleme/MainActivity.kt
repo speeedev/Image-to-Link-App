2e88684560762403b7618f1fbba1eb8cd645f55a
Emir <89213834+emirybolat@users.noreply.github.com>
2022-07-27 23:04:34 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/resim_yukleme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.image.upload.service
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:resim_yukleme/screens/HomePage.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
